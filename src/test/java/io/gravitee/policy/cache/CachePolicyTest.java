775ad6908ab55404d63469027c6bd4a4fd50573e
Nicolas Geraud <nicolas.geraud@gmail.com>
2022-01-27 11:15:10 +0100

    fix: choose the method to cache and add a response condition
    fix gravitee-io/issues#6980

--- src/test/java/io/gravitee/policy/cache/CachePolicyTest.java
+++ src/test/java/io/gravitee/policy/cache/CachePolicyTest.java
@@ -15,18 +15,35 @@
  */
 package io.gravitee.policy.cache;
 
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import io.gravitee.common.http.HttpHeaders;
+import io.gravitee.common.http.HttpMethod;
+import io.gravitee.common.util.LinkedMultiValueMap;
+import io.gravitee.el.TemplateContext;
+import io.gravitee.el.TemplateEngine;
+import io.gravitee.gateway.api.ExecutionContext;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.Response;
 import io.gravitee.gateway.api.proxy.ProxyResponse;
+import io.gravitee.policy.api.PolicyChain;
+import io.gravitee.policy.cache.configuration.CachePolicyConfiguration;
+import io.gravitee.resource.api.ResourceManager;
+import io.gravitee.resource.cache.api.Cache;
+import io.gravitee.resource.cache.api.CacheResource;
+import java.util.Collections;
 import java.util.HashMap;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
@@ -36,7 +53,22 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class CachePolicyTest {
 
     @Mock
-    protected ProxyResponse response;
+    protected ExecutionContext executionContext;
+
+    @Mock
+    protected PolicyChain policyChain;
+
+    @Mock
+    protected Response response;
+
+    @Mock
+    protected Request request;
+
+    @Mock
+    protected ProxyResponse proxyResponse;
+
+    @Mock
+    protected CachePolicyConfiguration cachePolicyConfiguration;
 
     @Before
     public void init() {
@@ -55,9 +87,9 @@ public class CachePolicyTest {
             }
         );
 
-        when(response.headers()).thenReturn(headers);
+        when(proxyResponse.headers()).thenReturn(headers);
 
-        long timeToLive = CachePolicy.timeToLiveFromResponse(response);
+        long timeToLive = CachePolicy.timeToLiveFromResponse(proxyResponse);
         Assert.assertEquals(300, timeToLive);
     }
 
@@ -73,9 +105,9 @@ public class CachePolicyTest {
             }
         );
 
-        when(response.headers()).thenReturn(headers);
+        when(proxyResponse.headers()).thenReturn(headers);
 
-        long timeToLive = CachePolicy.timeToLiveFromResponse(response);
+        long timeToLive = CachePolicy.timeToLiveFromResponse(proxyResponse);
         Assert.assertEquals(600, timeToLive);
     }
 
@@ -91,9 +123,167 @@ public class CachePolicyTest {
             }
         );
 
-        when(response.headers()).thenReturn(headers);
+        when(proxyResponse.headers()).thenReturn(headers);
 
-        long timeToLive = CachePolicy.timeToLiveFromResponse(response);
+        long timeToLive = CachePolicy.timeToLiveFromResponse(proxyResponse);
         Assert.assertEquals(-1, timeToLive);
+    }
+
+    @Test
+    public void shouldFailIfNoCacheResource() {
+        when(request.headers()).thenReturn(new HttpHeaders());
+        when(request.parameters()).thenReturn(new LinkedMultiValueMap());
+        when(request.method()).thenReturn(HttpMethod.GET);
+        when(cachePolicyConfiguration.getCacheName()).thenReturn("cache");
+        ResourceManager rm = mock(ResourceManager.class);
+        CacheResource cr = mock(CacheResource.class);
+        when(executionContext.getComponent(ResourceManager.class)).thenReturn(rm);
+        when(rm.getResource("cache", CacheResource.class)).thenReturn(cr);
+        when(cr.getCache(executionContext)).thenReturn(null);
+
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+        cachePolicy.onRequest(request, response, executionContext, policyChain);
+
+        verify(policyChain, times(1)).failWith(any());
+    }
+
+    @Test
+    public void shouldUseCacheOnGETByDefault() {
+        when(request.headers()).thenReturn(new HttpHeaders());
+        when(request.parameters()).thenReturn(new LinkedMultiValueMap());
+        when(request.method()).thenReturn(HttpMethod.GET);
+        ResourceManager rm = mock(ResourceManager.class);
+        CacheResource cr = mock(CacheResource.class);
+        Cache cache = mock(Cache.class);
+        when(executionContext.getComponent(ResourceManager.class)).thenReturn(rm);
+        when(rm.getResource(any(), eq(CacheResource.class))).thenReturn(cr);
+        when(cr.getCache(executionContext)).thenReturn(cache);
+
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+        cachePolicy.onRequest(request, response, executionContext, policyChain);
+
+        verify(policyChain, times(1)).doNext(any(), any());
+        verify(policyChain, never()).failWith(any());
+        verify(executionContext, times(1)).setAttribute(eq(ExecutionContext.ATTR_INVOKER), any(CachePolicy.CacheInvoker.class));
+    }
+
+    @Test
+    public void shouldNotUseCacheOnPOSTByDefault() {
+        when(request.headers()).thenReturn(new HttpHeaders());
+        when(request.parameters()).thenReturn(new LinkedMultiValueMap());
+        when(request.method()).thenReturn(HttpMethod.POST);
+
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+        cachePolicy.onRequest(request, response, executionContext, policyChain);
+
+        verify(policyChain, times(1)).doNext(any(), any());
+        verify(policyChain, never()).failWith(any());
+        verify(executionContext, never()).setAttribute(eq(ExecutionContext.ATTR_INVOKER), any(CachePolicy.CacheInvoker.class));
+    }
+
+    @Test
+    public void shouldUseCacheOnPOST() {
+        when(request.headers()).thenReturn(new HttpHeaders());
+        when(request.parameters()).thenReturn(new LinkedMultiValueMap());
+        when(request.method()).thenReturn(HttpMethod.POST);
+        when(cachePolicyConfiguration.getMethods()).thenReturn(Collections.singletonList(HttpMethod.POST));
+        ResourceManager rm = mock(ResourceManager.class);
+        CacheResource cr = mock(CacheResource.class);
+        Cache cache = mock(Cache.class);
+        when(executionContext.getComponent(ResourceManager.class)).thenReturn(rm);
+        when(rm.getResource(any(), eq(CacheResource.class))).thenReturn(cr);
+        when(cr.getCache(executionContext)).thenReturn(cache);
+
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+        cachePolicy.onRequest(request, response, executionContext, policyChain);
+
+        verify(policyChain, times(1)).doNext(any(), any());
+        verify(policyChain, never()).failWith(any());
+        verify(executionContext, times(1)).setAttribute(eq(ExecutionContext.ATTR_INVOKER), any(CachePolicy.CacheInvoker.class));
+    }
+
+    @Test
+    public void shouldNotUseCacheOnGET() {
+        when(request.headers()).thenReturn(new HttpHeaders());
+        when(request.parameters()).thenReturn(new LinkedMultiValueMap());
+        when(request.method()).thenReturn(HttpMethod.GET);
+        when(cachePolicyConfiguration.getMethods()).thenReturn(Collections.singletonList(HttpMethod.POST));
+
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+        cachePolicy.onRequest(request, response, executionContext, policyChain);
+
+        verify(policyChain, times(1)).doNext(any(), any());
+        verify(policyChain, never()).failWith(any());
+        verify(executionContext, never()).setAttribute(eq(ExecutionContext.ATTR_INVOKER), any(CachePolicy.CacheInvoker.class));
+    }
+
+    @Test
+    public void shouldTrueIfEvaluateWithNoCondition() {
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+
+        Boolean evaluate = ReflectionTestUtils.invokeMethod(
+            cachePolicy,
+            "evaluate",
+            executionContext,
+            Mockito.mock(ProxyResponse.class),
+            null
+        );
+
+        assertTrue(evaluate);
+    }
+
+    @Test
+    public void shouldTrueIfEvaluateWithEmptyCondition() {
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+
+        Boolean evaluate = ReflectionTestUtils.invokeMethod(
+            cachePolicy,
+            "evaluate",
+            executionContext,
+            Mockito.mock(ProxyResponse.class),
+            ""
+        );
+
+        assertTrue(evaluate);
+    }
+
+    @Test
+    public void shouldTrueIfEvaluationTrue() {
+        TemplateEngine templateEngine = mock(TemplateEngine.class);
+        TemplateContext templateContext = mock(TemplateContext.class);
+        when(executionContext.getTemplateEngine()).thenReturn(templateEngine);
+        when(templateEngine.getTemplateContext()).thenReturn(templateContext);
+        when(templateEngine.getValue(eq("true"), eq(Boolean.class))).thenReturn(Boolean.TRUE);
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+
+        Boolean evaluate = ReflectionTestUtils.invokeMethod(
+            cachePolicy,
+            "evaluate",
+            executionContext,
+            Mockito.mock(ProxyResponse.class),
+            "true"
+        );
+
+        assertTrue(evaluate);
+    }
+
+    @Test
+    public void shouldFalseIfEvaluationFalse() {
+        TemplateEngine templateEngine = mock(TemplateEngine.class);
+        TemplateContext templateContext = mock(TemplateContext.class);
+        when(executionContext.getTemplateEngine()).thenReturn(templateEngine);
+        when(templateEngine.getTemplateContext()).thenReturn(templateContext);
+        when(templateEngine.getValue(eq("false"), eq(Boolean.class))).thenReturn(Boolean.FALSE);
+        CachePolicy cachePolicy = new CachePolicy(cachePolicyConfiguration);
+
+        Boolean evaluate = ReflectionTestUtils.invokeMethod(
+            cachePolicy,
+            "evaluate",
+            executionContext,
+            Mockito.mock(ProxyResponse.class),
+            "false"
+        );
+
+        assertFalse(evaluate);
     }
 }
